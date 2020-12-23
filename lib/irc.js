matrix-org/node-irc

commit 19daa5b286c4baa4221cf81cb5967d3686767410

Add new option encodingFallback to guess non-utf-8 encoding correctly.

lib/irc.js
@@ -22,6 +22,7 @@ const dns  = require('dns');
 const net  = require('net');
 const tls  = require('tls');
 const util = require('util');
+const isUtf8 = require('is-utf8');
 const { EventEmitter } = require('events');
 
 const colors = require('./colors');
@@ -67,6 +68,7 @@ function Client(server, nick, opt) {
         channelPrefixes: '&#',
         messageSplit: 512,
         encoding: false,
+        encodingFallback: null,
         onNickConflict: function(maxLen) { // maxLen may be undefined if not known
             if (typeof (self.opt.nickMod) == 'undefined')
                 self.opt.nickMod = 0;
@@ -905,10 +907,6 @@ Client.prototype.connect = function(retryCount, callback) {
     self.conn.requestedDisconnect = false;
     self.conn.setTimeout(1000 * 180);
 
-    if (!self.opt.encoding) {
-        self.conn.setEncoding('utf8');
-    }
-
     let buffer = Buffer.alloc(0);
 
     self.conn.addListener('data', function(chunk) {
@@ -1269,11 +1267,11 @@ Client.prototype.ctcp = function(to, type, text) {
 
 Client.prototype.convertEncoding = function(str) {
     var self = this, out = str;
+    const Iconv = require('iconv').Iconv;
 
     if (self.opt.encoding) {
         try {
             const detectCharset = require('detect-character-encoding');
-            const Iconv = require('iconv').Iconv;
             const charset = detectCharset(str);
             if (!charset) {
                 throw Error("No charset detected");
@@ -1284,6 +1282,18 @@ Client.prototype.convertEncoding = function(str) {
             if (self.opt.debug) {
                 util.log('\u001b[01;31mERROR: ' + err + '\u001b[0m');
                 util.inspect({ str: str, charset: charset });
+            }
+        }
+    } else if (self.opt.encodingFallback) {
+        try {
+            if (!isUtf8(str)) {
+                const converter = new Iconv(self.opt.encodingFallback, "UTF-8")
+                out = converter.convert(str)
+            }
+        } catch (err) {
+            if (self.opt.debug) {
+                util.log('\u001b[01;31mERROR: ' + err + '\u001b[0m');
+                util.inspect({ str: str, encodingFallback: self.opt.encodingFallback });
             }
         }
     }

commit 096f0eb3d28f70bb9e8755e44ab77aaee915c555

Make iconv dependency optional again.
Although iconv is listed in optionalDependencies, it's always required
in Client.prototype.convertEncoding function in lib/irc.js file,
even if neither "encoding" nor "encodingFallback" options are defined.

This error was introduced in 19daa5b2 when require('iconv') was
erroneously moved to the top of that function to avoid code duplication.

This change makes iconv module not required again when encoding and
encodingFallback options are not defined.

Signed-off-by: Oleg Girko <ol@infoserver.lv>

lib/irc.js
@@ -1277,7 +1277,6 @@ Client.prototype.ctcp = function(to, type, text) {
 
 Client.prototype.convertEncoding = function(str) {
     var self = this, out = str;
-    const Iconv = require('iconv').Iconv;
 
     if (self.opt.encoding) {
         try {
@@ -1286,6 +1285,7 @@ Client.prototype.convertEncoding = function(str) {
             if (!charset) {
                 throw Error("No charset detected");
             }
+            const Iconv = require('iconv').Iconv;
             const converter = new Iconv(charset.encoding, self.opt.encoding);
             out = converter.convert(str);
         } catch (err) {
@@ -1297,6 +1297,7 @@ Client.prototype.convertEncoding = function(str) {
     } else if (self.opt.encodingFallback) {
         try {
             if (!isValidUTF8(str)) {
+                const Iconv = require('iconv').Iconv;
                 const converter = new Iconv(self.opt.encodingFallback, "UTF-8")
                 out = converter.convert(str)
             }