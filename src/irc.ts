950165d6a9ab7ccbde95e868acf953d9d8f68118
Will Hunt <will@half-shot.uk>
2021-05-25 15:52:38 +0100

    remove the trace

--- src/irc.ts
+++ src/irc.ts
@@ -1107,7 +1107,6 @@ export class Client extends EventEmitter {
         if (this.opt.secure) {
             let secureOpts: tls.ConnectionOptions = {
                 ...connectionOpts,
-                enableTrace: true,
                 rejectUnauthorized: !this.opt.selfSigned,
             }
 
